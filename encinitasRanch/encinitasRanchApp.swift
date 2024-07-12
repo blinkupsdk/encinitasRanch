f539f19ef1191cc8946e4b89e0f76481865e20bd
Chris <chrismuto@gmail.com>
2024-07-12 12:55:21 -0700

    initial encinitas ranch connect launch

--- encinitasRanch/encinitasRanchApp.swift
+++ encinitasRanch/encinitasRanchApp.swift
@@ -9,20 +9,20 @@ import SwiftUI
 import bLinkup
 
 @main
-struct swiftTemplateApp: App {
+struct encinitasRanchApp: App {
     init() {
         bLinkup.configure()
     }
     var body: some Scene {
         WindowGroup {
-            BlinkupRootScreen(customer: .init(id: "0-gbcEdQqg8G9dB_XSm-JkLZ5ZC4uIwfHMiy-nbT3sk=",
-                                              name: "Kit Carson Demo"),
+            BlinkupRootScreen(customer: .init(id: "92ppXgmdlRJ2kkTolS2JD2KCBXehREbpybhsOHqblnw=",
+                                              name: "Encinitas Ranch Connect"),
                               branding: .init(primary: UIColor(red: 0, green: 0.14, blue: 0.3, alpha: 1),
                                               secondary: UIColor(red: 0.8, green: 0.05, blue: 0.2, alpha: 1),
                                               fontName: nil,
                                               logo: "client-logo",
-                                              name: "Kit Carson Demo",
-                                              title: "Demo Connect")
+                                              name: "Encinitas Ranch Connect",
+                                              title: "Connect at Encinitas Ranch")
             )
         }
     }
